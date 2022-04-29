d2ce267c9ea5f05bd70ed3b5bf74597e663a0bc5
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-29 08:52:41 +0100

    un projet de test

---
+++ src/main/java/com/sip/ams/DevopsGkApplication.java
@@ -0,0 +1,14 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GK");
+		SpringApplication.run(DevopsGkApplication.class, args);
+	}
+
+}
